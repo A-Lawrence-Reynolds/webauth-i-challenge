d34010665f2d1c157829044bf67bd2bdbf3b94fe
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2019-12-17 13:12:37 -0800

    added sessions and cookies

--- api/server.js
+++ api/server.js
@@ -1,12 +1,39 @@
 const express = require("express");
-
-const apiRouter = require("./api-router.js");
-const configureMiddleware = require("./configure-middleware.js");
-
+const helmet = require("helmet");
+const cors = require("cors");
+const sessions = require("express-session"); // <<<<< install express-session
+const KnexSessionStore = require("connect-session-knex")(sessions);
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 const server = express();
+const knex = require("../data/dbConfig.js");
+const sessionConfiguration = {
+  // session storage options
+  name: "Subaru", // default would be sid
+  secret: "love , its what makes a subaru a subaru", // used for encryption (must be an environment variable)
+  saveUninitialized: true, // has implications with GDPR laws
+  resave: false,
 
-configureMiddleware(server);
-
-server.use("/api", apiRouter);
-
+  store: new KnexSessionStore({
+    knex,
+    tablename: "sessions",
+    createtable: true,
+    clearInterval: 1000 * 60 * 10
+  }),
+  // cookie options
+  cookie: {
+    maxAge: 1000 * 60 * 10, // 10 mins in milliseconds
+    secure: false, // if false the cookie es sent over http, if true only sent over https
+    httpOnly: true // if true JS cannot access the cookie
+  }
+};
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(sessions(sessionConfiguration)); // add a req.session object
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
 module.exports = server;
